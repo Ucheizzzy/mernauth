df29e5e4917b3257c2b4a7a43395113920877209
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-10 15:55:37 +0100

    created all the routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,45 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  About,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Profile,
+  Register,
+} from './pages'
+
 const App = () => {
-  return <div className='text-3xl text-red-600'>App</div>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Landing />,
+        },
+        {
+          path: 'about',
+          element: <About />,
+        },
+        {
+          path: 'login',
+          element: <Login />,
+        },
+        {
+          path: 'register',
+          element: <Register />,
+        },
+        {
+          path: 'profile',
+          element: <Profile />,
+        },
+      ],
+    },
+  ])
+  return <RouterProvider router={router}>App</RouterProvider>
 }
 
 export default App
