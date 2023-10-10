48fc557ce73f4c9cfd01d11e33ee59f2d6330a59
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-10 16:29:57 +0100

    created the navbar section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,25 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className='flex justify-between items-center p-3 mx-auto max-w-6xl'>
+      <Link to='/'>
+        <h1 className='font-bold text-slate-200 text-2xl'>MERN Auth</h1>
+      </Link>
+
+      <ul className='text-slate-200 flex gap-4'>
+        <Link to='/'>
+          <li>Home</li>
+        </Link>
+        <Link to='about'>
+          <li>About</li>
+        </Link>
+        <Link to='login'>
+          <li>Sign In</li>
+        </Link>
+      </ul>
+    </div>
+  )
 }
 
 export default Navbar
