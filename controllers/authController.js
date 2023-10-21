0d7c9bb878dcb53f1ce56825bf9a1213b9894a2c
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-21 22:20:27 +0100

    login done, working on logout endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 import { StatusCodes } from 'http-status-codes'
 import User from '../models/userModel.js'
-import { hashedPassword } from '../utils/passwordUtils.js'
+import { comparedPassword, hashedPassword } from '../utils/passwordUtils.js'
+import { BadRequestError } from '../errors/customeErrors.js'
+import { createJWT } from '../utils/tokenUtils.js'
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0
@@ -12,5 +14,19 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-  res.status(StatusCodes.OK).json({ msg: ' I am the login route' })
+  const user = await User.findOne({ email: req.body.email })
+  const isValidator =
+    user && (await comparedPassword(req.body.password, user.password))
+  if (!isValidator) throw new BadRequestError('invalid credentials')
+  //set the payload for the jwt created
+  const token = createJWT({ userId: user._id, role: user.role })
+  //set up cookie and pass the token
+  const oneDay = 1000 * 60 * 60 * 24
+  res.cookie('token', token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === 'production',
+  })
+
+  res.status(StatusCodes.OK).json({ msg: 'user logged in successfully' })
 }
--- utils/passwordUtils.js
+++ utils/passwordUtils.js
@@ -6,3 +6,8 @@ export const hashedPassword = async (password) => {
   const hashedPassword = await bcrypt.hash(password, salt)
   return hashedPassword
 }
+
+export const comparedPassword = async (password, hashedPassword) => {
+  const isMatch = await bcrypt.compare(password, hashedPassword)
+  return isMatch
+}
