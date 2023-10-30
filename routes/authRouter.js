627fe930fed2eabf8dd2a44d64987bfcf44537e8
Mr.Lion <uchechukwuobiakor@gmail.com>
2023-10-30 18:28:03 +0100

    fixing google auth bug

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -11,16 +11,9 @@ import {
   validateLoginInput,
   validateRegisterInput,
 } from '../middlewares/validationMiddleware.js'
-import rateLimiter from 'express-rate-limit'
 
-const apiLimiter = rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
-  message: { msg: 'IP rate limit exceeded, retry in 15 minutes.' },
-})
-
-router.route('/register').post(apiLimiter, validateRegisterInput, register)
-router.route('/login').post(apiLimiter, validateLoginInput, login)
+router.route('/register').post(validateRegisterInput, register)
+router.route('/login').post(validateLoginInput, login)
 router.route('/google').post(google)
 router.route('/logout').get(logout)
 
